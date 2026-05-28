8380017a1d8650ef511ce4fd3fca6d6d2c775d14
unknown <theodormunanga@gmail.com>
2026-05-28 05:12:41 +0100

    chemin import fix

--- src/pages/BotsPage.jsx
+++ src/pages/BotsPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import api from "../services/api";
+import api from "../../services/api";
 
 export default function BotsPage({ setPage }) {
   const [balance, setBalance] = useState(0);
